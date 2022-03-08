aa3116129066b24d446d12db5d4d81fe279e0c21
livinya <livinya.20200694@iit.ac.lk>
2022-03-08 14:16:30 +0530

    Allow camera on. yes no? without background image

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div  styles={{ backgroundImage:`url(${bgimg.jpg})` }}>
+      <h1>This is red car</h1>
+    
+    <div className = "App">
+      <h1>would you like to turn on your webcam?</h1>
+      <button className="yesBtn">YES</button>
+      <button className="noBtn">NO</button>
+   </div> 
+   </div>
+  )
+};
+  
+export default App;
+ 
